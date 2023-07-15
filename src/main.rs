24c13e39ab3197981ec740c9843b103750ad5559
Idooru <shere1765@gmail.com>
2023-07-15 22:30:02 +0900

    Floats

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use std::mem::size_of;
-
 fn main() {
-    // char 4 bytes
-    println!("Size of a char: {} bytes", std::mem::size_of::<char>());
+   let my_number = 9_u8;
+   let other_number = 1_000_000_000u64;
 
-    println!("Size of string containing 'a': {}", "a".len());  // ascii code
-    println!("Size of string containing 'ㄱ': {}", "ㄱ".len());   // uni code
-    println!("Size of string containing 'ㄱㄱㄱ': {}", "ㄱㄱㄱ".len());   // uni code
-
-    let slice = "Hello!";
-    println!("Slice is {} bytes and also {} characters", slice.len(), slice.chars().count());
-    let slice2= "안녕!";
-    println!("Slice is {} bytes and also {} characters", slice2.len(), slice2.chars().count());
+   let my_number = 9.6;
+   let other_number = 9;
+   println!("{}", my_number as i32 + other_number);
+   println!("{}", my_number + other_number as f64);
 }
